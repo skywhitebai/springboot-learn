228293b792a742dd37ca4003cd4eba52afb89572
skywhitebai <183693833@qq.com>
2017-11-26 22:27:22 +0800

    springboot

---
+++ src/girl/src/main/java/com/sky/girl/GirlApplication.java
@@ -0,0 +1,12 @@
+package com.sky.girl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GirlApplication.class, args);
+	}
+}
---
+++ src/girl/src/main/java/com/sky/girl/GirlRepository.java
@@ -0,0 +1,6 @@
+package com.sky.girl;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GirlRepository extends JpaRepository<Girl,Integer> {
+}
